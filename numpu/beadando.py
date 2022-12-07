523cfbb79070aa0642601b836f5fa28c9c15d7e1
HuszarDora <dora.huszar@stud.uni-corvinus.hu>
2022-12-07 17:04:11 +0100

    Adatok szerkesztése

--- numpu/beadando.py
+++ numpu/beadando.py
@@ -1,12 +1,30 @@
 import pandas as pd
 import numpy as np
+from functools import reduce
 
-df_dba = pd.read_csv('DBA.csv')
-df_tlt = pd.read_csv('TLT.csv')
-df_vde = pd.read_csv('VDE.csv')
-df_xlv = pd.read_csv('XLV.csv')
-df_xme = pd.read_csv('XME.csv')
+# csak azért hogy meglegyen az eredeti táblázat
+df_dba_original = pd.read_csv('DBA.csv')
+df_tlt_original = pd.read_csv('TLT.csv')
+df_vde_original = pd.read_csv('VDE.csv')
+df_xlv_original = pd.read_csv('XLV.csv')
+df_xme_original = pd.read_csv('XME.csv')
 
-print(1)
-print(2)
+df_dba = df_dba_original[['Date', 'Adj Close']]
+df_tlt = df_tlt_original[['Date', 'Adj Close']]
+df_vde = df_vde_original[['Date', 'Adj Close']]
+df_xlv = df_xlv_original[['Date', 'Adj Close']]
+df_xme = df_xme_original[['Date', 'Adj Close']]
+
+df_dba = df_dba.rename(columns={'Adj Close': 'Adj Close_dba'})
+df_tlt = df_tlt.rename(columns={'Adj Close': 'Adj Close_tlt'})
+df_vde = df_vde.rename(columns={'Adj Close': 'Adj Close_vde'})
+df_xlv = df_xlv.rename(columns={'Adj Close': 'Adj Close_xlv'})
+df_xme = df_xme.rename(columns={'Adj Close': 'Adj Close_xme'})
+
+filenames = [df_dba, df_tlt, df_vde, df_xlv, df_xme]
+
+
+df_merge = reduce(lambda left, right: pd.merge(left, right, on=['Date'], how='inner'), filenames)
+
+
 pass
